017562cf659504f3ea59d17984f1d486753a1be7
Haowei A ZHANG <Haowei.a.Zhang@alcatel-sbell.com.cn>
2017-10-09 16:30:54 +0800

    add performance file

---
+++ performance_ana/memory_profile.py
@@ -0,0 +1,18 @@
+#coding=GBK
+'''
+Created on 2017Äê10ÔÂ9ÈÕ
+
+@author: haoweizh
+'''
+import random
+
+
+@profile
+def random_sort2(n):
+    l = [random.random() for i in range(n)]
+    l.sort()
+    return l
+
+
+if __name__ == "__main__":
+    random_sort2(2000000)
